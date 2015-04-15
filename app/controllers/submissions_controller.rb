ec44b35eeebf74713c33dc9247933eddd613b443
Andrew Mutz <andrew.mutz@gmail.com>
2015-04-15 15:44:47 -0700

    Making login required to access the main page.
    Adding the users email address to the header.

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,4 +1,5 @@
 class SubmissionsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_submission, only: [:show, :edit, :update, :destroy]
 
   # GET /submissions
