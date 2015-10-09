c4b60a0f4f34d3ba5ef9e0358a471d44505ddaff
mqchau <quanqchau@gmail.com>
2015-10-09 10:43:41 -0700

    got the first form to ask user where to park

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  root 'submissions#index'
+  #root 'submissions#index'
 
   resources :comments
 
   resources :submissions
 
   resources :communities
+
+  get 'welcome/index'
+  root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
